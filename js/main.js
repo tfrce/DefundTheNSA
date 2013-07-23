c9d4e65934f91765bfeec08c082c50e7cef95fe7
Beau Gunderson <beau@beaugunderson.com>
2013-07-23 00:22:01 -0700

    Zipcode WIP

--- js/main.js
+++ js/main.js
@@ -3,64 +3,42 @@
 var CONGRESS_URL = 'http://congress.api.sunlightfoundation.com';
 var API_KEY = '8d0caa0295254038a5b61878a06d80ec';
 
+var TWEET_MESSAGE = 'Lorem ipsum, etc., etc.,';
+
 function getLegislators(zip, cb) {
   $.getJSON(CONGRESS_URL + '/legislators/locate?apikey=' + API_KEY + '&zip=' +
     zip, function (legislators) {
-    cb(legislators);
+    cb(legislators.results);
   });
 }
 
 function submitZipcode() {
   getLegislators($('#zipcode').val(), function (legislators) {
     console.log(legislators);
-  });
+    var htmlFragment = '';
 
-  /*
-  for (var i = 0; i < a.length; i++) {
-    if (a[i].legislator['twitter_id'] != '') {
-      handles_avail = 1;
-      message = "";
-      messagef = '';
-      urlmessage = encodeURIComponent(messagef);
-
-      html_fragment = html_fragment + '<div class="legtweet clearfix" style="margin-top:20px;font-size:14px;"><div class="">' +
-        message + '</div><div class="span2" id="'+
-        a[i].legislator['twitter_id'] +
+    legislators.filter(function (legislator) {
+      return legislator.twitter_id !== '';
+    }).forEach(function (legislator) {
+      htmlFragment += '<div class="legtweet clearfix" style="margin-top: 20px; font-size: 14px;"><div class="">' +
+        TWEET_MESSAGE + '</div><div class="span2" id="' +
+        legislator.twitter_id +
         '"><a href="https://twitter.com/intent/tweet?screen_name=' +
-        a[i].legislator['twitter_id'] +
+        legislator.twitter_id +
         '" class="twitter-mention-button legislatortweet" url="http://defundthensa.com" data-lang="en" data-count="none" data-size="large" data-counturl="http://defundthensa.com">Tweet to @' +
-        a[i].legislator['twitter_id']+'</a></div></div>';
-    }
-  }
+        legislator.twitter_id + '</a></div></div>';
+    });
 
-  if (handles_avail == 1) {
-    $('div#legtweets').removeClass('hide');
-    $('.get-legislators').parent().removeClass("error");
-  }
+    $('#tweets').html(htmlFragment);
 
-  if (handles_avail == 0) {
-    $('div#legtweets').addClass('hide');
-    $('.get-legislators').parent().addClass("error");
-  }
-
-  $("#legtweets").html(html_fragment);
-  */
-
-  $.getScript("http://platform.twitter.com/widgets.js");
+    $.getScript("http://platform.twitter.com/widgets.js");
+  });
 }
 
 $(function () {
-  $('#submit-zipcode').click(function (e) {
-    e.preventDefault();
-
+  $('form').submit(function () {
     submitZipcode();
-  });
 
-  $('#zipcode').keypress(function (e) {
-    if (e.which === 13) {
-      e.preventDefault();
-
-      submitZipcode();
-    }
+    return false;
   });
 });
