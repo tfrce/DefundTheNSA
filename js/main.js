41891d7141449f4e7bd0a6c62b549890fa5d28ad
Beau Gunderson <beau@beaugunderson.com>
2013-07-23 02:26:41 -0700

    Add logic for no results

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*globals $:true, _:true*/
+/*globals $:true, _:true, _trackEvent:true*/
 
 var CONGRESS_URL = 'http://congress.api.sunlightfoundation.com';
 var API_KEY = '8d0caa0295254038a5b61878a06d80ec';
@@ -24,6 +24,8 @@ function submitZipcode() {
 
     var tweetTemplate = $('#tweet-template').html();
     var phoneTemplate = $('#phone-template').html();
+
+    var notFoundTemplate = $('#not-found-template').html();
 
     var tweetFragments = '';
     var phoneFragments = '';
@@ -51,8 +53,12 @@ function submitZipcode() {
       });
     });
 
+    phoneFragments = phoneFragments || notFoundTemplate;
+    tweetFragments = tweetFragments || notFoundTemplate;
+
     $('#phones .list').html(phoneFragments);
     $('#tweets .list').html(tweetFragments);
+
     $('#tweets, #phones').show();
 
     $.getScript("http://platform.twitter.com/widgets.js");
