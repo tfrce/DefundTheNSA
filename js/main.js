14ada2d1037f3c2a9f090a36e7a2fb30de5f6fde
Thomas Davis <thomasalwyndavis@gmail.com>
2013-07-23 09:13:56 +0000

    postcode submit

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function submitZipcode() {
 $(function () {
   $('form').submit(function () {
     submitZipcode();
-
+    _trackEvent('action', 'zipcode-lookup', 'defund', 1, false);
     return false;
   });
 });
